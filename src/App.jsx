56bf9f9a3808066c76c217fbaf1dda5dbe7d82b6
yi-chai <u2004609@siswa.um.edu.my>
2024-05-27 16:10:36 +0800

    27 May 24

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
 
 function App() {
   return (
     <main>
       <section>
         <h2>Why work with us?</h2>
-        <Accordion className="accordion"></Accordion>
+        <Accordion className="accordion">
+          <AccordionItem
+            className="accordion-item"
+            id="experience"
+            title="We got 20 years of experience"
+          >
+            <article>
+              <p>You can't go wrong with us.</p>
+              <p>We are the best in the business!</p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            className="accordion-item"
+            id="tour-guide"
+            title="Love working with tour guides"
+          >
+            <article>
+              <p>Tour guides are important</p>
+              <p>So you don't get lose</p>
+            </article>
+          </AccordionItem>
+        </Accordion>
       </section>
     </main>
   );
